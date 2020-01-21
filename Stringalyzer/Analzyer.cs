8656c94e5b70b67f05df1fa97fc6acc627b72351
ead <ead900@gmail.com>
2020-01-21 18:00:16 -0500

    add count all words fn

---
+++ Stringalyzer/Analzyer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Stringalyzer
+{
+    public class Analyzer
+    {
+        private readonly string _input;
+        private readonly string[] _input_tokens;
+        private readonly char[] _delimiters = new char[] { ' ', ',', '.', '!', '?', '\n', '(', ')', ':', ';', }; 
+
+        public Analyzer(string toAnalyze)
+        {
+            _input = toAnalyze;
+            _input_tokens = _input.Split(_delimiters, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private int? _totalWords;
+        public int TotalWords
+        {
+            get => CountAllWords();
+            private set => _totalWords = value;
+        }
+
+        private int CountAllWords()
+        {
+            if (_totalWords == null)
+            {
+                _totalWords = _input_tokens.GetLength(0);
+            }
+            return (int)_totalWords;
+        }
+    }
+}
--- Stringalyzer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Stringalyzer
-{
-    public class Class1
-    {
-    }
-}
